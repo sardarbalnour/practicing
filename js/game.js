35262241ee7b78031c2873177133550e6e0eeac5
Sardar Balnour <balnour.sardar@gmail.com>
2025-12-21 16:43:52 +0330

    formattedData

--- js/game.js
+++ js/game.js
@@ -3,11 +3,27 @@ const URL =
 
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
+let formattedData = null;
+
+const formatData = (questionData) => {
+  // console.log(questionData);
+  const result = questionData.map((item) => {
+    const questionObject = { question: item.question };
+    const answers = [...item.incorrect_answers];
+    const correctAnswerIndex = Math.floor(Math.random() * 4);
+    answers.splice(correctAnswerIndex, 0, item.correct_answer);
+    // console.log(answers, correctAnswerIndex);
+    questionObject.answers = answers;
+    questionObject.correctAnswerIndex = correctAnswerIndex;
+    return questionObject;
+  });
+  return result;
+};
 
 const fetchData = async () => {
   const response = await fetch(URL);
   const json = await response.json();
-  console.log(json);
+  formattedData = formatData(json.results);
   start();
 };
 
