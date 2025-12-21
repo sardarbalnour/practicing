9f3abe6e9153e17848abf6e803cb57b1bd47dcfe
Sardar Balnour <balnour.sardar@gmail.com>
2025-12-21 16:44:20 +0330

    module

--- js/game.js
+++ js/game.js
@@ -1,24 +1,11 @@
+import formatData from "./helper.js";
+
 const URL =
   "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple";
 
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 let formattedData = null;
-
-const formatData = (questionData) => {
-  // console.log(questionData);
-  const result = questionData.map((item) => {
-    const questionObject = { question: item.question };
-    const answers = [...item.incorrect_answers];
-    const correctAnswerIndex = Math.floor(Math.random() * 4);
-    answers.splice(correctAnswerIndex, 0, item.correct_answer);
-    // console.log(answers, correctAnswerIndex);
-    questionObject.answers = answers;
-    questionObject.correctAnswerIndex = correctAnswerIndex;
-    return questionObject;
-  });
-  return result;
-};
 
 const fetchData = async () => {
   const response = await fetch(URL);
---
+++ js/helper.js
@@ -0,0 +1,16 @@
+const formatData = (questionData) => {
+  // console.log(questionData);
+  const result = questionData.map((item) => {
+    const questionObject = { question: item.question };
+    const answers = [...item.incorrect_answers];
+    const correctAnswerIndex = Math.floor(Math.random() * 4);
+    answers.splice(correctAnswerIndex, 0, item.correct_answer);
+    // console.log(answers, correctAnswerIndex);
+    questionObject.answers = answers;
+    questionObject.correctAnswerIndex = correctAnswerIndex;
+    return questionObject;
+  });
+  return result;
+};
+
+export default formatData;
