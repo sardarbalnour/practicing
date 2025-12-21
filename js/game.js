80fe5f8716ae87356569e0ca4d126e63c1afc80a
Sardar Balnour <balnour.sardar@gmail.com>
2025-12-21 11:51:43 +0330

    fetch data and strat function

--- js/game.js
+++ js/game.js
@@ -1,1 +1,19 @@
-console.log("starttt");
+const URL =
+  "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple";
+
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+
+const fetchData = async () => {
+  const response = await fetch(URL);
+  const json = await response.json();
+  console.log(json);
+  start();
+};
+
+const start = () => {
+  loader.style.display = "none";
+  container.style.display = "block";
+};
+
+window.addEventListener("load", fetchData);
