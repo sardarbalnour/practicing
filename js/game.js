1e541afedac352978db46d92c86a922469befa89
Sardar Balnour <balnour.sardar@gmail.com>
2025-12-22 19:51:17 +0330

    score

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 import formatData from "./helper.js";
 
+const CORRECT_BONUS = 10;
 const URL =
   "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple";
 
@@ -7,10 +8,13 @@ const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 const questionText = document.getElementById("question-text");
 const answerList = document.querySelectorAll(".answer-text");
+const scoreText = document.getElementById("score");
 
 let formattedData = null;
 let questionIndex = 0;
 let correctAnswer = null;
+let score = 0;
+let isAccepted = true;
 
 const fetchData = async () => {
   const response = await fetch(URL);
@@ -37,12 +41,17 @@ const showQuestion = () => {
 };
 
 const checkAnswer = (event, index) => {
+  if (!isAccepted) return;
+  isAccepted = false;
+
   const isCorrect = index === correctAnswer ? true : false;
   if (isCorrect) {
     event.target.classList.add("correct");
+    score += CORRECT_BONUS;
+    scoreText.innerText = score;
   } else {
     event.target.classList.add("incorrect");
-    answerList[correctAnswer].classList.add("correct")
+    answerList[correctAnswer].classList.add("correct");
   }
 };
 
